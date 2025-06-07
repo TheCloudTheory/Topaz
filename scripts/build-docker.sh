0b4f36969135dea5b73c0f692a973428142a3035
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-06-07 14:27:22 +0200

    Slightly different method of building Dockerfile which is aware of different platforms

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./publish
 dotnet publish ./Topaz.CLI/Topaz.CLI.csproj -c Release -r linux-arm64 -o ./publish
-docker build -f ./Topaz.CLI/Dockerfile -t topaz/cli --platform linux/arm64 .
+docker build -f ./Topaz.CLI/Dockerfile -t topaz/cli --platform linux/arm64 --build-arg TARGETPLATFORM=linux-arm64 .
