23fa31a9a0e8da8c24f781b1a8a46a3c77e23426
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-06-08 11:19:06 +0200

    Added TestContainers to the example project

--- Examples/Topaz.Example.Dotnet/Program.cs
+++ Examples/Topaz.Example.Dotnet/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DotNet.Testcontainers.Builders;
+
+Console.WriteLine("Topaz Example - .NET");
+
+// Create a builder for Topaz container image and all the ports
+// which are exposed by the emulator. Note you don't need to expose
+// every port available.
+var container = new ContainerBuilder()
+    .WithImage("thecloudtheory/topaz-cli:v1.0.61-alpha")
+    .WithPortBinding(8890)
+    .WithPortBinding(8899)
+    .WithPortBinding(8898)
+    .WithPortBinding(8897)
+    .WithPortBinding(8891)
+    .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8890).ForPort(8899).ForPort(8898).ForPort(8897).ForPort(8891)))
+    .Build();
+    
+await container.StartAsync()
+    .ConfigureAwait(false);
+    
+    
