40ce0e135492ad9dfbca67d6ac6df66a6611c1d5
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-06-08 18:30:58 +0200

    Including missing asset via some MSBuild magic

--- Examples/Topaz.Example.Dotnet/Program.cs
+++ Examples/Topaz.Example.Dotnet/Program.cs
@@ -9,29 +9,35 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("Topaz Example - .NET");
-        
-        var subscriptionId = Guid.NewGuid();
-        var subscriptionName = "topaz.example";
-
         // Create a builder for Topaz container image and all the ports
         // which are exposed by the emulator. Note you don't need to expose
         // every port available.
         var container = new ContainerBuilder()
-            .WithImage("thecloudtheory/topaz-cli:v1.0.61-alpha")
+            .WithImage("thecloudtheory/topaz-cli:v1.0.76-alpha")
             .WithPortBinding(8890)
             .WithPortBinding(8899)
             .WithPortBinding(8898)
             .WithPortBinding(8897)
             .WithPortBinding(8891)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8890).ForPort(8899).ForPort(8898).ForPort(8897).ForPort(8891)))
             .Build();
+        
+        try
+        {
+            Console.WriteLine("Topaz Example - .NET");
+        
+            var subscriptionId = Guid.NewGuid();
+            var subscriptionName = "topaz.example";
     
-        await container.StartAsync()
-            .ConfigureAwait(false);
+            await container.StartAsync()
+                .ConfigureAwait(false);
 
-        await CreateSubscription(subscriptionId, subscriptionName);
-        CreateResourceGroup();
+            await CreateSubscription(subscriptionId, subscriptionName);
+            CreateResourceGroup();
+        }
+        finally
+        {
+            await container.StopAsync();
+        }
     }
 
     private static void CreateResourceGroup()
