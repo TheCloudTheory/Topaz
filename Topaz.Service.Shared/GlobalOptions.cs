5d2c1d0c2bb590c769259a3dd3c9ef265e0b528e
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-06-18 23:52:31 +0200

    Added possibility to bring your own certificate, added test suite for Azure CLI tests

--- Topaz.Service.Shared/GlobalOptions.cs
+++ Topaz.Service.Shared/GlobalOptions.cs
@@ -3,4 +3,6 @@ namespace Topaz.Service.Shared;
 public sealed class GlobalOptions
 {
     public Guid? TenantId { get; init; }
+    public string? CertificateFile { get; set; }
+    public string? CertificateKey { get; set; }
 }
