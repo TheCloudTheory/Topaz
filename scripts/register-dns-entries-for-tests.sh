5f80d85d1b6233a8e8b434e866985d8e1d39ae31
kamil-mrzyglod <kamil@thecloudtheory.com>
2025-10-25 21:01:09 +0200

    Ensuring the script is run from the correct location

--- scripts/register-dns-entries-for-tests.sh
+++ scripts/register-dns-entries-for-tests.sh
@@ -3,6 +3,8 @@
 # Registers the required DNS entries in /etc/hosts for local development tests.
 # Usage: ./register-dns-entries-for-tests.sh
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 DNS_ENTRIES_STORAGE=("blobstoragetests" "azurestoragetests" "tablestoragetests")
 
 for entry in "${DNS_ENTRIES_STORAGE[@]}"; do
