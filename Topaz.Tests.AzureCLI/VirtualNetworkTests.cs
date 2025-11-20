40f2a4e5b86eaf14de97e9bfeb3378e32b425b4c
kamil-mrzyglod <kamil@thecloudtheory.com>
2025-11-20 15:51:51 +0100

    Added one more test with opposite condition

--- Topaz.Tests.AzureCLI/VirtualNetworkTests.cs
+++ Topaz.Tests.AzureCLI/VirtualNetworkTests.cs
@@ -7,4 +7,19 @@ public class VirtualNetworkTests : TopazFixture
     {
         await RunAzureCliCommand("az network vnet create --location westeurope --name my-vnet --resource-group some-not-existing-resource-group", null, 3);
     }
+    
+    [Test]
+    public async Task VirtualNetworkTests_WhenResourceGroupExists_VirtualNetworkMustBeCreated()
+    {
+        await RunAzureCliCommand("az group create -l westeurope -n rg-vnet", null, 0);
+        await RunAzureCliCommand("az network vnet create --location westeurope --name my-vnet --resource-group rg-vnet",
+            response =>
+            {
+                Assert.Multiple(() =>
+                {
+                    Assert.That(response["newVNet"]!["name"]!.GetValue<string>(), Is.EqualTo("my-vnet"));
+                    Assert.That(response["newVNet"]!["location"]!.GetValue<string>(), Is.EqualTo("westeurope"));
+                });
+            }, 0);
+    }
 }
