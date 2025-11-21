4e75dd114f1761a4b25aa728d14065d1c7b0898a
kamil-mrzyglod <kamil@thecloudtheory.com>
2025-11-21 14:01:01 +0100

    Added an example MCP tool setting up the emulator

---
+++ Topaz.MCP/Tools/SetupTopazTool.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel;
+using DotNet.Testcontainers.Builders;
+using ModelContextProtocol.Server;
+using Topaz.Shared;
+
+namespace Topaz.MCP.Tools;
+
+[McpServerToolType]
+[Description("Helps in setting up Topaz locally.")]
+public class SetupTopazTool
+{
+    [McpServerTool]
+    [Description("Runs and configures Topaz locally as a container")]
+    public async Task RunTopazAsContainer(
+        [Description("Configures log level to be used by Topaz")]
+        LogLevel logLevel = LogLevel.Information,
+        [Description("Image tag to use when running the emulator")]
+        string version = "v1.0.270-alpha")
+    {
+        var container = new ContainerBuilder()
+            .WithImage($"thecloudtheory/topaz-cli:{version}")
+            .WithPortBinding(8890)
+            .WithPortBinding(8899)
+            .WithPortBinding(8898)
+            .WithPortBinding(8897)
+            .WithPortBinding(8891)
+            .WithName("topaz.local.dev")
+            .WithCommand("start", "--skip-dns-registration", "--log-level", logLevel.ToString())
+            .Build();
+
+        await container.StartAsync()
+            .ConfigureAwait(false);
+    }
+}
