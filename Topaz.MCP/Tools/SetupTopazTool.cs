3a415883acfbec4f7e1f5eaf565dc645beb297a5
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-12-18 22:44:24 +0100

    Installed NuGet annotation package, updated default version

--- Topaz.MCP/Tools/SetupTopazTool.cs
+++ Topaz.MCP/Tools/SetupTopazTool.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using DotNet.Testcontainers.Builders;
+using JetBrains.Annotations;
 using ModelContextProtocol.Server;
 using Topaz.Shared;
 
@@ -7,15 +8,17 @@ namespace Topaz.MCP.Tools;
 
 [McpServerToolType]
 [Description("Helps in setting up Topaz locally.")]
+[UsedImplicitly]
 public class SetupTopazTool
 {
     [McpServerTool]
     [Description("Runs and configures Topaz locally as a container")]
+    [UsedImplicitly]
     public async Task RunTopazAsContainer(
         [Description("Configures log level to be used by Topaz")]
         LogLevel logLevel = LogLevel.Information,
         [Description("Image tag to use when running the emulator")]
-        string version = "v1.0.270-alpha")
+        string version = "v1.0.299-alpha")
     {
         var container = new ContainerBuilder()
             .WithImage($"thecloudtheory/topaz-cli:{version}")
