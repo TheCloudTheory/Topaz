0754faa2c0fc9789c859fffbd124bba7a48ff108
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-12-19 08:54:29 +0100

    Handling a command to stop the container

--- Topaz.MCP/Tools/SetupTopazTool.cs
+++ Topaz.MCP/Tools/SetupTopazTool.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using DotNet.Testcontainers.Builders;
 using JetBrains.Annotations;
+using ModelContextProtocol;
 using ModelContextProtocol.Server;
 using Topaz.Shared;
 
@@ -11,16 +12,18 @@ namespace Topaz.MCP.Tools;
 [UsedImplicitly]
 public class SetupTopazTool
 {
+    private static DotNet.Testcontainers.Containers.IContainer? _container;
+    
     [McpServerTool]
     [Description("Runs and configures Topaz locally as a container")]
     [UsedImplicitly]
-    public async Task RunTopazAsContainer(
+    public static async Task RunTopazAsContainer(
         [Description("Configures log level to be used by Topaz")]
         LogLevel logLevel = LogLevel.Information,
         [Description("Image tag to use when running the emulator")]
         string version = "v1.0.299-alpha")
     {
-        var container = new ContainerBuilder()
+        _container = new ContainerBuilder()
             .WithImage($"thecloudtheory/topaz-cli:{version}")
             .WithPortBinding(8890)
             .WithPortBinding(8899)
@@ -31,7 +34,20 @@ public class SetupTopazTool
             .WithCommand("start", "--skip-dns-registration", "--log-level", logLevel.ToString())
             .Build();
 
-        await container.StartAsync()
+        await _container.StartAsync()
             .ConfigureAwait(false);
+    }
+    
+    [McpServerTool]
+    [Description("Stops Topaz which was previously run as container.")]
+    [UsedImplicitly]
+    public static async Task StopTopazContainer()
+    {
+        if (_container == null) throw new McpException("Can't stop Topaz if it wasn't started.");
+        
+        await _container.StopAsync()
+            .ConfigureAwait(false);
+
+        await _container.DisposeAsync().ConfigureAwait(false);
     }
 }
