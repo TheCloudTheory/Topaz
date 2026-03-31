2d75c65dd8e9a317f4f3bd8e5f82d73a186b22d0
Kamil Mrzygłód <kamil@thecloudtheory.com>
2026-03-31 10:16:59 +0200

    refactor: Improve formatting and readability in ResourceAuthorization classes and methods

--- Services/Topaz.Service.Authorization/AuthorizationResourceProvider.cs
+++ Services/Topaz.Service.Authorization/AuthorizationResourceProvider.cs
@@ -7,23 +7,32 @@ using Topaz.Shared;
 
 namespace Topaz.Service.Authorization;
 
-internal sealed class ResourceAuthorizationResourceProvider(ITopazLogger logger) : ResourceProviderBase<ResourceAuthorizationService>(logger);
-internal sealed class ResourceGroupAuthorizationResourceProvider(ITopazLogger logger) : ResourceProviderBase<ResourceGroupAuthorizationService>(logger);
-internal sealed class RoleAssignmentResourceProvider(ITopazLogger logger) : ResourceProviderBase<RoleAssignmentService>(logger);
-internal sealed class RoleDefinitionResourceProvider(ITopazLogger logger) : ResourceProviderBase<RoleDefinitionService>(logger)
+internal sealed class ResourceAuthorizationResourceProvider(ITopazLogger logger)
+    : ResourceProviderBase<ResourceAuthorizationService>(logger);
+
+internal sealed class ResourceGroupAuthorizationResourceProvider(ITopazLogger logger)
+    : ResourceProviderBase<ResourceGroupAuthorizationService>(logger);
+
+internal sealed class RoleAssignmentResourceProvider(ITopazLogger logger)
+    : ResourceProviderBase<RoleAssignmentService>(logger);
+
+internal sealed class RoleDefinitionResourceProvider(ITopazLogger logger)
+    : ResourceProviderBase<RoleDefinitionService>(logger)
 {
     private readonly ITopazLogger _logger = logger;
 
     public RoleDefinitionResource[] ListBuiltInRoles(SubscriptionIdentifier subscriptionIdentifier)
     {
-        _logger.LogDebug(nameof(RoleDefinitionResourceProvider), nameof(ListBuiltInRoles), "List built-in roles for `{0}` subscription.", subscriptionIdentifier);
-        
+        _logger.LogDebug(nameof(RoleDefinitionResourceProvider), nameof(ListBuiltInRoles),
+            "List built-in roles for `{0}` subscription.", subscriptionIdentifier);
+
         var definitions = new List<RoleDefinitionResource>();
         var rawFiles = Directory.EnumerateFiles("Data", "*.json", SearchOption.AllDirectories);
         foreach (var file in rawFiles)
         {
-            _logger.LogDebug(nameof(RoleDefinitionResourceProvider), nameof(ListBuiltInRoles), "Loading contents of a `{0}` file as role definition.", file);
-            
+            _logger.LogDebug(nameof(RoleDefinitionResourceProvider), nameof(ListBuiltInRoles),
+                "Loading contents of a `{0}` file as role definition.", file);
+
             var content = File.ReadAllText(file);
             var fileModel = JsonSerializer.Deserialize<RoleDefinition>(content, GlobalSettings.JsonOptions);
 
@@ -43,7 +52,7 @@ internal sealed class RoleDefinitionResourceProvider(ITopazLogger logger) : Reso
                 _logger.LogError($"Could not convert `{file}` to `{nameof(RoleDefinitionResource)}`: {ex.Message}");
             }
         }
-        
+
         return definitions.ToArray();
     }
 
@@ -53,16 +62,12 @@ internal sealed class RoleDefinitionResourceProvider(ITopazLogger logger) : Reso
             "Looking for built-in role by id `{0}`.", roleDefinitionIdentifier);
 
         var rawFiles = Directory.EnumerateFiles("Data", "*.json", SearchOption.AllDirectories);
-        foreach (var file in rawFiles)
-        {
-            var content = File.ReadAllText(file);
-            var fileModel = JsonSerializer.Deserialize<RoleDefinition>(content, GlobalSettings.JsonOptions);
-            if (fileModel == null) continue;
-
-            if (fileModel.Name != roleDefinitionIdentifier.Value &&
-                fileModel.Id?.Contains(roleDefinitionIdentifier.Value) != true) continue;
-
-            var properties = new RoleDefinitionResourceProperties
+        return (from fileModel in rawFiles.Select(File.ReadAllText)
+                .Select(content => JsonSerializer.Deserialize<RoleDefinition>(content, GlobalSettings.JsonOptions))
+                .OfType<RoleDefinition>()
+            where fileModel.Name == roleDefinitionIdentifier.Value ||
+                  fileModel.Id?.Contains(roleDefinitionIdentifier.Value) == true
+            let properties = new RoleDefinitionResourceProperties
             {
                 RoleName = fileModel.RoleName,
                 Description = fileModel.Description,
@@ -70,17 +75,11 @@ internal sealed class RoleDefinitionResourceProvider(ITopazLogger logger) : Reso
                 AssignableScopes = fileModel.AssignableScopes,
                 Permissions = fileModel.Permissions?.Select(p => new RoleDefinition.Permission
                 {
-                    Actions = p.Actions,
-                    NotActions = p.NotActions,
-                    DataActions = p.DataActions,
+                    Actions = p.Actions, NotActions = p.NotActions, DataActions = p.DataActions,
                     NotDataActions = p.NotDataActions
                 }).ToArray()
-            };
-
-            var roleId = fileModel.Id ?? $"/providers/Microsoft.Authorization/roleDefinitions/{fileModel.Name}";
-            return new RoleDefinitionResource(roleId, fileModel.Name!, properties);
-        }
-
-        return null;
+            }
+            let roleId = fileModel.Id ?? $"/providers/Microsoft.Authorization/roleDefinitions/{fileModel.Name}"
+            select new RoleDefinitionResource(roleId, fileModel.Name!, properties)).FirstOrDefault();
     }
 }
