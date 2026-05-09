7196dbb6e1628a7e820ce69e714e9fc4684b5b7f
Kamil Mrzygłód <kamil@thecloudtheory.com>
2026-05-09 14:23:23 +0200

    feat: Add GlobalTestFixture for E2E testing setup and teardown

--- Topaz.Tests/E2E/E2EFixture.cs
+++ Topaz.Tests/E2E/E2EFixture.cs
@@ -1,55 +1,6 @@
-using Topaz.Service.Shared;
-using Topaz.Shared;
-using TopazHost = Topaz.Host.Host;
-
 namespace Topaz.Tests.E2E;
 
+// Host startup is handled by GlobalTestFixture (Topaz.Tests namespace),
+// which covers all test namespaces in this assembly.
 [SetUpFixture]
-public class E2EFixture
-{
-    private static Task? _topaz;
-    private static readonly CancellationTokenSource CancellationTokenSource = new();
-
-    [OneTimeSetUp]
-    public async Task SetUp()
-    {
-        var logger = new PrettyTopazLogger();
-        var logLevel = Environment.GetEnvironmentVariable("CI") == "true" ? LogLevel.Information : LogLevel.Debug;
-        logger.SetLoggingLevel(logLevel);
-        logger.EnableLoggingToFile(true);
-
-        var host = new TopazHost(new GlobalOptions
-        {
-            EnableLoggingToFile = true,
-            EmulatorIpAddress = "127.0.0.1"
-        }, logger);
-
-        _topaz = host.StartAsync(CancellationTokenSource.Token);
-
-        await Task.Delay(1000);
-    }
-
-    [OneTimeTearDown]
-    public async Task TearDown()
-    {
-        await CancellationTokenSource.CancelAsync();
-
-        if (_topaz != null)
-        {
-            try
-            {
-                await _topaz.WaitAsync(TimeSpan.FromSeconds(2));
-            }
-            catch (OperationCanceledException)
-            {
-                // Expected when cancellation is triggered
-            }
-            catch (TimeoutException)
-            {
-                // Task didn't complete in time
-            }
-        }
-
-        CancellationTokenSource.Dispose();
-    }
-}
+public class E2EFixture;
